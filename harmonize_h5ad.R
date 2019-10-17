2fd1dd950ae0093e389b864d84c4477e12228624
Neal Smith <nealpsmith@bcb-data.partners.org>
2019-10-17 15:34:16 -0400

    intial commit.  The beginning of a command line tool to run harmony on PCs in an h5ad file

---
+++ harmonize_h5ad.R
@@ -0,0 +1,37 @@
+#!/usr/bin/env Rscript
+library(rhdf5)
+library(harmony)
+library(data.table)
+require(docopt)
+
+'Usage:
+   harmonize_h5ad.R [--help --h5 <h5ad> --var <var> --output <output>]
+
+Options:
+   --h5 h5ad that contains the PCs to be harmonized
+   --var variable to harmonize on (i.e the batch)
+   --output output file to save harmonized PCs
+ ]' -> doc
+opts <- docopt(doc)
+
+# Get the h5 file path
+h5_file <- opts$h5
+
+# Get the observations from the h5
+obs <- h5read(h5_file, "obs")
+
+# Get the PCA and transpose it
+pca <- t(h5read(h5_file, "obsm/X_pca"))
+
+
+set.seed(42)
+
+harmonized <- HarmonyMatrix(
+data_mat = pca,
+meta_data = obs,
+vars_use = opts$var,
+do_pca = FALSE,
+verbose = FALSE
+)
+
+write.csv(harmonized, file = opts$output)
